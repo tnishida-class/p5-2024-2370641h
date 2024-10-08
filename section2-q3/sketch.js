2e5ee9230a3984842402472b79c6c8e691b3913f
2370641h <2370641h@stu.kobe-u.ac.jp>
2024-10-08 17:04:34 +0900

    changed section2-3q

--- section2-q3/sketch.js
+++ section2-q3/sketch.js
@@ -17,6 +17,10 @@ function setup() {
   drawCircle(black, maxR);
   drawArcs(green, red, maxR * 0.8);
   // BLANK[1] (hint: drawArcs x 3, drawCircle x 1)
+  drawArcs()
+  drawArcs()
+  drawArcs()
+  drawCircle(green,maxR*0.1);
   drawCircle(red, maxR * 0.05);
 }
 
