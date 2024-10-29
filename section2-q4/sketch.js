c8db41dd61aa1c789a6d65c0647f2153a6904cba
2370641h <2370641h@stu.kobe-u.ac.jp>
2024-10-29 11:49:48 +0900

    3

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -1,33 +1,33 @@
 // ギリシャ国旗
-function setup() {
-  const blue = color(0, 51, 160);
-  const white =color(255,255,253);
-  createCanvas(270, 180);
+//function setup() {
+ // const blue = color(0, 51, 160);
+  //const white =color(255,255,253);
+  //createCanvas(270, 180);
   // noStroke();
-  background(255);
+  //background(255);
 
-  let d = height / 9; // 縞1本の太さ
-   noStroke();
-  for(let i = 0; i < 9; i++){
+  //let d = height / 9; // 縞1本の太さ
+   //noStroke();
+  //for(let i = 0; i < 9; i++){
     // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
-    if((i%2)===1){
-      fill(white);
-      rect(0, i * d, width, (i + 1) * d);
-    }else{
-      fill(blue);
-      rect(0, i * d, width, (i + 1) * d);
-    }
-  }
+    //if((i%2)===1){
+      //fill(white);
+      //rect(0, i * d, width, (i + 1) * d);
+    //}else{
+      //fill(blue);
+      //rect(0, i * d, width, (i + 1) * d);
+    //}
+  //}
 
-  fill(blue);
-  let size = d * 5;
-  rect(0, 0, size, size);
+  //fill(blue);
+  //let size = d * 5;
+  //rect(0, 0, size, size);
 
-  fill(255);
+  //fill(255);
   // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
-  rect(size/2-d*0.5,0,d,size);
-  rect(0,size/2-d*0.5,size,d);
-}
+  //rect(size/2-d*0.5,0,d,size);
+  //rect(0,size/2-d*0.5,size,d);
+//}
 
 
 function setup() {
--- section3-3/sketch.js
+++ section3-3/sketch.js
@@ -1,23 +1,59 @@
 // テキスト「キーボード操作に反応する」
 let x, y;
+let vx,vy;
+const g=1;
+const jump=20;
+const ground=20;
+const size=50
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
   x = width / 2;
   y = height / 2;
+  vx=0;
+  vy=0;
 }
 
 function draw(){
   background(160, 192, 255);
-  ellipse(x, y, 50);
+  line(0,height/2,width,height/2);
+  ellipse(x, y, size,size);
+  
+  x += vx;
+  y += vy;
+
   if(keyIsDown(LEFT_ARROW)){ x -= 5; }
   if(keyIsDown(RIGHT_ARROW)){ x += 5; }
-  if(keyIsDown(UP_ARROW)){ y -= 5; }
-  if(keyIsDown(DOWN_ARROW)){ y += 5; }
+ // if(keyIsDown(UP_ARROW)){ y -= 5; }
+ // if(keyIsDown(DOWN_ARROW)){ y += 5; }
   if(keyIsDown("A".charCodeAt(0))){ x+= 10; }
-  if(keyIsDown(" ".charCodeAt(0))){ x-= 10; }
-}
 
+  if(y<height-y-size){
+    vy+=g;
+  }else{
+    vy=0;
+    y=height-y-size;
+  }
+  
+  if(keyIsDown("F".charCodeAt(0))&&(keyIsDown(LEFT_ARROW)||keyIsDown(UP_ARROW))){
+    vx = -10;
+    //vy = -10;
+  } else if (keyIsDown("F".charCodeAt(0))&&(keyIsDown(RIGHT_ARROW)||keyIsDown(DOWN_ARROW))){
+    vx = 10;
+    //vy = 10;
+  }  else {
+    vx = 0;
+    //vy = 0;
+  }
+  x=constrain(x,0,width);
+  y=constrain(y,0,height);
+}
+function keyPressed(){
+  if(key==" "){
+    if(y>=height-y-size)
+      vy=-jump;
+  }
+}
 // イベントハンドラを使用するパターン
 // function keyPressed(){
 //   if(keyCode == LEFT_ARROW){ x -= 5; }
